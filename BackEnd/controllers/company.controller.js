b4208e3bde3ea16eecd410bd572fb22b48115350
Rajvi Dhameliya <https://github.com/RajviDhameliyarajvidhameliya004@gmail.com>
2025-12-06 23:58:22 +0530

    Fix utils folder case and import paths for Render

--- BackEnd/controllers/company.controller.js
+++ BackEnd/controllers/company.controller.js
@@ -1,6 +1,6 @@
 import { Company } from "../models/company.model.js";
-import getDataUri from "../utils/datauri.js";
-import cloudinary from "../utils/cloudinary.js";
+import getDataUri from "../Utils/dataUri.js";
+import cloudinary from "../Utils/cloudinary.js";
 
 export const registerCompany = async (req, res) => {
     try {
