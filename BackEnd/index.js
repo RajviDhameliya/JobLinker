67f1adc287c9f824870ab833368c2ddb213af305
Rajvi Dhameliya <https://github.com/RajviDhameliyarajvidhameliya004@gmail.com>
2025-12-07 00:54:19 +0530

    Final Fix

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -17,9 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: [
+    "http://localhost:5173",                     // local dev
+    "https://job-linker-lovat.vercel.app",      // your Vercel domain
+  ],
+  credentials: true,
+};
+
 
 app.use(cors(corsOptions));
 
